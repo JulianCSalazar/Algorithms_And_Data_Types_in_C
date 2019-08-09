4cfa415bf12b0e2c383e515d1f1df54608bf4155
Julian Salazar <jucsalaz@ucsc.edu>
2019-08-08 23:24:37 -0700

    Added merge sort to list of sorting algorithms.

--- Sorting_Algorithms/Sorting_Algorithms.ino
+++ Sorting_Algorithms/Sorting_Algorithms.ino
@@ -4,6 +4,10 @@
 
 #define MAIN setup
 
+/**********************************************************
+ * SORTING FUNCTIONS
+ **********************************************************/
+
 void bubbleSort(int A[], int a_len)
 {
   int i, j;
@@ -50,6 +54,67 @@ void insertionSort(int A[], int a_len)
   Serial.println("Finished insertion sorting");
 }
 
+void mergeSort(int A[], int a_len)
+{
+  mergeRecursion(A, 0, a_len-1);
+}
+
+void mergeRecursion(int A[], int leftPoint, int rightPoint)
+{
+  if (leftPoint < rightPoint) {
+    int midPoint = (rightPoint + leftPoint) / 2;
+    mergeRecursion(A, leftPoint, midPoint);
+    mergeRecursion(A, midPoint+1, rightPoint);
+    merge(A, leftPoint, midPoint, rightPoint);
+  }
+}
+
+void merge(int A[], int l, int m, int r)
+{
+
+  int subArray1Len = m - l + 1;
+  int subArray2Len = r - m;
+  int subArray1[subArray1Len];
+  int subArray2[subArray2Len];
+
+  //Now we copy the data of the data in A to our sub-Arrays
+  int i;
+  for ( i = 0; i < subArray1Len; i++) {
+    subArray1[i] = A[l + i];
+  }
+  for ( i = 0; i < subArray2Len; i++) {
+    subArray2[i] = A[m + i + 1];
+  }
+
+  i = 0;
+  int j = 0;
+  int k = l;
+  
+  while ((i < subArray1Len) && (j < subArray2Len)) {
+    if (subArray1[i] < subArray2[j]) {
+      A[k] = subArray1[i];
+      i++;
+    } else {
+      A[k] = subArray2[j];
+      j++;
+    }
+    k++;
+  }
+
+  while (j < subArray2Len) {
+    A[k] = subArray2[j];
+    j++;
+    k++;
+  }
+  while (i < subArray1Len) {
+    A[k] = subArray1[i];
+    i++;
+    k++;
+  }
+}
+/**********************************************************
+ * HELPER FUNCTIONS
+ **********************************************************/
 void swap(int A[], int i1, int i2) 
 {
   int temp = A[i1];
@@ -85,6 +150,10 @@ void printArray(int A[], int a_len)
   Serial.println(" ");
 }
 
+/**********************************************************
+ * MAIN CODE
+ **********************************************************/
+
 void MAIN() 
 {
   Serial.begin(9600);
@@ -96,6 +165,7 @@ void MAIN()
   //bubbleSort(myRandomArray, arrayLength);
   //selectionSort(myRandomArray, arrayLength);
   //insertionSort(myRandomArray, arrayLength);
+  mergeSort(myRandomArray, arrayLength);
   printArray(myRandomArray, arrayLength);
 }
 
